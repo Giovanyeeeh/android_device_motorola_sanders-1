d1fda07f0d4b8a4f4030f2bf0afe0f906a93f01d
Giovanyeeeh <86035674+Giovanyeeeh@users.noreply.github.com>
2022-05-21 00:28:52 +0000

    Update and rename arrow_sanders.mk to spark_sanders.mk

--- spark_sanders.mk
+++ spark_sanders.mk
@@ -20,7 +20,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_n_mr1.mk)
 
 # Inherit some common ArrowOS stuff.
-$(call inherit-product, vendor/arrow/config/common.mk)
+$(call inherit-product, vendor/spark/config/common.mk)
 
 # Inherit from sanders device
 $(call inherit-product, device/motorola/sanders/device.mk)
@@ -32,8 +32,7 @@ TARGET_BOOT_ANIMATION_RES := 1080
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := sanders
-DEVICE_MAINTAINER := Ashwatthama
-PRODUCT_NAME := arrow_sanders
+PRODUCT_NAME := spark_sanders
 PRODUCT_BRAND := motorola
 PRODUCT_MANUFACTURER := motorola
 PRODUCT_MODEL := Moto G (5S) Plus
